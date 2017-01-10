0e628c43c22ff20f4a2b2d04f4fb6110df687d51
anirban2017 <anirbanmit1980@gmail.com>
2017-01-10 20:33:52 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,11 @@
+package Assignment3_3;
+
+public class example {
+	int x; int y;
+	 String name;
+	 public static void main(String args[]) {
+	 example pnt = new example();
+	 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+	}
+
+}
